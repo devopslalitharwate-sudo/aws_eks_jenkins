a2aab384d6b13b3733be466855a28fd3c67c4f63
devopslalitharwate-sudo <devops.lalitharwate@gmail.com>
2025-12-16 16:42:03 +0530

    r

--- eks-setup/main.tf
+++ eks-setup/main.tf
@@ -11,7 +11,6 @@ module "vpc" {
   private_subnets         = var.private_subnets
   public_subnets          = var.public_subnets
   enable_dns_hostnames    = true
-  map_public_ip_on_launch = true
   enable_nat_gateway      = true
   single_nat_gateway      = true
 
