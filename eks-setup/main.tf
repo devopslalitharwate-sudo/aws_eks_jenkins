8587a7e68f907f76e23ce6b05215efa37ea4fd5f
devopslalitharwate-sudo <devops.lalitharwate@gmail.com>
2025-12-17 15:24:57 +0530

    Update main.tf

--- eks-setup/main.tf
+++ eks-setup/main.tf
@@ -1,7 +1,6 @@
 #vpc
 
 module "vpc" {
-
   source = "terraform-aws-modules/vpc/aws"
 
   name = "eks-vpc"
@@ -10,31 +9,15 @@ module "vpc" {
   azs                     = data.aws_availability_zones.available.names
   private_subnets         = var.private_subnets
   public_subnets          = var.public_subnets
+
+  enable_dns_support      = true   # 🔥 ADD THIS
   enable_dns_hostnames    = true
+
   enable_nat_gateway      = true
   single_nat_gateway      = true
-
-  tags = {
-    Name        = "eks-vpc"
-    Terraform   = "true"
-    Environment = "dev"
-  }
-
-  public_subnet_tags = {
-    Name                                   = "eks-public-subnets"
-    "kubernetes.io/cluster/my-eks-cluster" = "shared"
-    "kubernetes.io/role/elb"               = 1
-  }
-
-  private_subnet_tags = {
-    Name                                   = "eks-private-subnets"
-    "kubernetes.io/cluster/my-eks-cluster" = "shared"
-    "kubernetes.io/role/internal-elb"      = "1"
-  }
+  ...
 }
 
-
-#eks
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
@@ -45,22 +28,16 @@ module "eks" {
   vpc_id             = module.vpc.vpc_id
   subnet_ids         = module.vpc.private_subnets
 
-endpoint_public_access = true
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
 
   eks_managed_node_groups = {
     nodes = {
-
       instance_types = ["t3.medium"]
-
       min_size     = 1
       max_size     = 3
       desired_size = 2
     }
-  }
-
-  tags = {
-    Environment = "dev"
-    Terraform   = "true"
   }
 }
 
